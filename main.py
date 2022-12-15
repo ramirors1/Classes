b96bc70810c5bb5ba3510646e8373cc28a6e8f58
ramirors1 <ramirezr@protonmail.com>
2022-12-15 07:49:04 -0600

    first commit

---
+++ main.py
@@ -0,0 +1,26 @@
+class User:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+user_1 = User("001", "Chewy")
+
+#print(user_1.username, user_1.followers)
+
+user_2 = User("002", "Brocka")
+
+#print(user_2.username, user_2.followers)
+
+user_1.follow(user_2)
+print(user_1.followers)
+print(user_1.following)
+print(user_2.followers)
+print(user_2.following)
